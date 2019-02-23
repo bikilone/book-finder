bd6e149d935e4ceb10f61b37ed73b7f433dbaf97
bikilone <milosbirovljevic@gmail.com>
2019-02-23 20:28:47 +0100

    Updates

---
+++ precache-manifest.47e8637be8177d8a9a69401de32a9997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "b2d2c8779ddfe24168d7",
+    "url": "/book-finder/static/js/main.b2d2c877.chunk.js"
+  },
+  {
+    "revision": "88ffe54300011357c7e7",
+    "url": "/book-finder/static/js/2.88ffe543.chunk.js"
+  },
+  {
+    "revision": "b2d2c8779ddfe24168d7",
+    "url": "/book-finder/static/css/main.4d235ac4.chunk.css"
+  },
+  {
+    "revision": "88ffe54300011357c7e7",
+    "url": "/book-finder/static/css/2.a4160f6d.chunk.css"
+  },
+  {
+    "revision": "472ae8bf3612306727581f4d3c3db37d",
+    "url": "/book-finder/index.html"
+  }
+];
