6a93a76df622b572cd50691b465006e3718230de
bikilone <milosbirovljevic@gmail.com>
2019-02-28 08:59:43 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/book-finder/precache-manifest.7bcc2f931f362214505d38e9027aedbc.js"
+  "/book-finder/precache-manifest.3199cb1226d2c8826f40d4fba7a9c5c3.js"
 );
 
 workbox.clientsClaim();
